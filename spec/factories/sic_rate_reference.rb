191a04f0ca523c6047efaa40c7df5721c56b0747
venkatbhattiprolu <vbhat847@gmail.com>
2017-05-03 14:23:38 -0400

    added factory

---
+++ spec/factories/sic_rate_reference.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :sic_rate_reference do
+		sic '0111'
+		hios_id '82569'
+		cost_ratio 1.000
+	 	applicable_year 2017
+	end
+end
--- spec/models/sic_rate_reference_spec.rb
+++ spec/models/sic_rate_reference_spec.rb
@@ -1,60 +1,16 @@
 require 'rails_helper'
 
-describe SicRateReference, "with proper validations" do
-  let(:sic) { "0111" }
-  let(:hios_id) { "82569" }
-  let(:cost_ratio) { 1.000 }
-  let(:applicable_year) { 2017 }
+RSpec.describe SicRateReference, type: :model do
+  subject { SicRateReference.new }
+
+
+  it "has a valid factory" do
+    expect(create(:sic_rate_reference)).to be_valid
+  end
+
   
-
-  let(:sic_rate_reference_params) {
-    {
-      sic: sic,
-      hios_id: hios_id,
-      cost_ratio: cost_ratio,
-      applicable_year: applicable_year
-    }
-  }
-
-  subject { SicRateReference.new(sic_rate_reference_params) }
-
-  before :each do
-    subject.valid?
-  end
-
-  context "with no arguments" do
-    let(:sic_rate_reference_params) { {} }
-    it "should not be valid" do
-      expect(subject.valid?).to be_falsey
-    end
-  end
-
-  context "with empty sic" do
-    let(:sic) { nil }
-    it "is not a valid sic rate reference" do
-      expect(subject.errors[:sic].any?).to be_truthy
-    end
-  end
-
-  context "with no hios_id" do
-    let(:hios_id) { nil }
-    it "is not a valid sic rate reference" do
-      expect(subject.errors[:hios_id].any?).to be_truthy
-    end
-  end
-
-  context "with no cost_ratio" do
-    let(:cost_ratio) { nil }
-    it "is not a valid sic rate reference" do
-      expect(subject.errors[:cost_ratio].any?).to be_truthy
-    end
-  end
-
-  context "with no applicable_year" do
-    let(:applicable_year) { nil }
-    it "is not valid sic rate reference" do
-      expect(subject.errors[:applicable_year].any?).to be_truthy
-    end
-  end
-
+  it { is_expected.to validate_presence_of :sic}
+  it { is_expected.to validate_presence_of :hios_id}
+  it { is_expected.to validate_presence_of :cost_ratio}
+  it { is_expected.to validate_presence_of :applicable_year}
 end
