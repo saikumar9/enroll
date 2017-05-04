83d39d5a3b1d7939c0ac77be90b30c0f2ee9bc2a
Prem Shaganti <prem.shaganti@dc.gov>
2017-05-04 10:53:23 -0400

    It will properly display view for broker_agency profile creation process

--- app/controllers/broker_agencies/profiles_controller.rb
+++ app/controllers/broker_agencies/profiles_controller.rb
@@ -10,7 +10,7 @@ class BrokerAgencies::ProfilesController < ApplicationController
   before_action :check_general_agency_profile_permissions_assign, only: [:assign, :update_assign, :clear_assign_for_employer, :assign_history]
   before_action :check_general_agency_profile_permissions_set_default, only: [:set_default_ga]
 
-  layout 'single_column'
+ # layout 'single_column'
 
   def index
     @broker_agency_profiles = BrokerAgencyProfile.all
