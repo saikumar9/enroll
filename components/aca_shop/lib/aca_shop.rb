954f3d79312b757ddbc22f413ebc74e00d3dedb5
Dan Thomas <dan.thomas@dc.gov>
2017-05-06 16:56:22 -0400

    Remove reference to aca-individual component

--- components/aca_shop/lib/aca_shop.rb
+++ components/aca_shop/lib/aca_shop.rb
@@ -1,7 +1,8 @@
 require "aca/shop/engine"
 require "rails/all"
 require "jquery-rails"
-require "aca-individual"
+
+# require "aca-individual"
 
 require File.expand_path("../monkey_patches/engine.rb", __FILE__)
 
