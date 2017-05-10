9cd0838e3c923060aeb52e80f94579be13b8973a
Bill Transue <transue@gmail.com>
2017-05-10 07:19:21 -0400

    Update components' test.sh

--- components/aca-individual/test.sh
+++ components/aca-individual/test.sh
@@ -2,21 +2,9 @@
 
 exit_code=0
 
-echo "*** Running teaser engine specs"
+echo "*** Running ACA::Individual engine specs"
 bundle install  --jobs=3 --retry=3 | grep Installing
-bundle exec rake db:create 
-bundle exec rake db:migrate
-RAILS_ENV=test bundle exec rake db:create 
-RAILS_ENV=test bundle exec rake db:migrate
-bundle exec rspec spec/controllers
-exit_code+=$?
-
-echo "*** Running teaser engine request specs"
-bundle exec rspec spec/features
-exit_code+=$?
-
-echo "*** Running teaser engine javascript specs"
-bundle exec rake app:jasmine:ci
+bundle exec rspec
 exit_code+=$?
 
 exit $exit_code
