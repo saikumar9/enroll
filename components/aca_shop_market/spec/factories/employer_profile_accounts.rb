823277502192d9ec544fefda035cd33da4cbb564
Dan Thomas <dan.thomas@dc.gov>
2017-05-24 07:22:31 -0400

    Initial commit

---
+++ components/aca_shop_market/spec/factories/employer_profile_accounts.rb
@@ -0,0 +1,9 @@
+module AcaShopMarket
+  FactoryGirl.define do
+    factory :employer_profile_account do
+      next_premium_due_on TimeKeeper.date_of_record.end_of_month + 1.day
+      next_premium_amount 5489.97
+    end
+
+  end
+end
