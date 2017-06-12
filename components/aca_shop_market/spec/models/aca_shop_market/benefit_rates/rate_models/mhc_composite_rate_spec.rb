4500c31aa89cd6692db762e9cf3e1445ad43e743
Dan Thomas <dan.thomas@dc.gov>
2017-06-12 17:04:39 -0400

    initialize benefit_rates model framework

---
+++ components/aca_shop_market/spec/models/aca_shop_market/benefit_rates/rate_models/mhc_composite_rate_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module AcaShopMarket
+  RSpec.describe BenefitRates::RateModels::MhcCompositeRate, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
