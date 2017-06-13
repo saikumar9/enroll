2433e80143853cd1f80a0d83147d9ba312ad79ee
Brian Weiner <brian@320ny.com>
2017-06-13 17:34:43 -0400

    Revert "Creating a new schedulable event"

--- config/routes.rb
+++ config/routes.rb
@@ -41,14 +41,6 @@ Rails.application.routes.draw do
       get :find_sep, on: :collection
     end
 
-    resources :scheduled_events do
-      collection do
-        get 'current_events'
-        get 'delete_current_event'
-        get 'list'
-      end
-    end
-
     resources :hbx_profiles do
       root 'hbx_profiles#show'
 
@@ -88,7 +80,6 @@ Rails.application.routes.draw do
         get :add_sep_form
         get :hide_form
         get :show_sep_history
-        get :calender_index
       end
 
       member do
