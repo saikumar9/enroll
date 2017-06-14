8c570c642ab755a2452a6f6e6ad8b42740e38c5a
Dan Thomas <dan.thomas@dc.gov>
2017-06-14 10:47:09 -0400

    Passing spec

--- components/transport_gateway/app/models/transport_gateway/message.rb
+++ components/transport_gateway/app/models/transport_gateway/message.rb
@@ -1,14 +1,39 @@
 require 'uri'
+
 module TransportGateway
   class Message
 
-    attr_accessor :from, :to, :body
+    attr_accessor :from, :to, :body
 
-    def initialize(from, to, body)
+    def initialize(from, to, body, envelope_header = {}, options = {})
       @from = from
       @to   = URI.parse(to)
       @body = body
+
+      # Canonical Vocabulary "EnvelopeHeaderType" attributes
+      parse_envelope_header(envelope_header) unless envelope_header.empty?
+
+      parse_options(options) unless options.empty?
     end
-    
+
+    private 
+
+    def parse_envelope_header(envelope_header)
+      @hbx_id                         = envelope_header[:hbx_id] || nil
+      @submitted_timestamp            = envelope_header[:submitted_timestamp] || nil
+      @authorization                  = envelope_header[:authorization] || nil
+      @service_status_type            = envelope_header[:service_status_type] || nil
+      @message_id                     = envelope_header[:message_id] || nil
+      @originating_service            = envelope_header[:originating_service] || nil
+      @reply_to                       = envelope_header[:reply_to] || nil
+      @fault_to                       = envelope_header[:fault_to] || nil
+      @correlation_id                 = envelope_header[:correlation_id] || nil
+      @application_header_properties  = envelope_header[:application_header_properties] || nil
+    end
+
+    def parse_options(options)
+      options.each { |k,v| instance_variable_set("@#{k}", v) }
+    end
+
   end
 end
--- components/transport_gateway/spec/models/transport_gateway/message_spec.rb
+++ components/transport_gateway/spec/models/transport_gateway/message_spec.rb
@@ -2,6 +2,54 @@ require 'rails_helper'
 
 module TransportGateway
   RSpec.describe Message, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+
+    let(:from)  { "foo@example.com" }
+    let(:to)    { "bar@example.com" }
+    let(:body)  { "lorum ipsum" }
+
+    context ".new" do
+      context "with no arguments" do
+
+        it "should raise an argument error" do
+          expect{ Message.new() }.to raise_error(ArgumentError) 
+        end
+      end
+
+      context "with a unparseable URI in 'to' argument" do
+        let(:invalid_uri)  { 7463 }
+
+        it "should raise Invalid URI error" do
+          expect{ Message.new(from, invalid_uri, body) }.to raise_error(URI::InvalidURIError) 
+        end
+      end
+
+      context "with empty 'from' argument" do
+        it "should not raise an error" do
+          expect{Message.new("", to, body)}.not_to raise_error 
+        end
+      end
+
+      context "with valid arguments" do
+        let(:params)  {valid_params}
+        let(:message_with_valid_arguments) { Message.new(from, to, body) }
+        let(:to_uri)  { URI.parse(to) }
+
+        it "should not raise an error" do
+          expect{message_with_valid_arguments}.not_to raise_error 
+        end
+
+        it "should set the instance vars" do
+          expect(message_with_valid_arguments.from).to eq from
+          expect(message_with_valid_arguments.body).to eq body
+        end
+
+        it "should cast the 'to' argument as a URI" do
+          expect(message_with_valid_arguments.to).to eq to_uri
+        end
+
+      end
+
+    end
+
   end
 end
