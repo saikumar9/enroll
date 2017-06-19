7e8625958e970b86b1bab4fff4809d1ba44fcf59
Dan Thomas <dan.thomas@dc.gov>
2017-06-19 13:03:06 -0400

    Configure Webmock and byebug gems

--- components/transport_gateway/spec/rails_helper.rb
+++ components/transport_gateway/spec/rails_helper.rb
@@ -8,6 +8,9 @@ require 'database_cleaner'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'factory_girl_rails'
+require 'webmock/rspec'
+require 'pry'
+WebMock.disable_net_connect!(allow_localhost: true)
 
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -35,12 +38,12 @@ require 'rspec/rails'
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
