8b1f9036e2856979187ecd078012dc92edf79674
Trey Evans <lewis.r.evans@gmail.com>
2017-06-20 15:42:02 -0400

    working on datepickers.

--- app/assets/angular2/app/new_plan_year.component.ts
+++ app/assets/angular2/app/new_plan_year.component.ts
@@ -3,6 +3,7 @@ import { BenefitPackage } from './benefit_package';
 import { NewPlanYearOptions } from './new_plan_year_options';
 import { DatePipe } from '@angular/common';
 import * as Moment from "moment";
+import { UUID } from 'angular2-uuid';
 
 @Component({
 	selector: "[data-angular2-new-plan-year-form]",
@@ -15,6 +16,7 @@ export class NewPlanYearComponent {
 	open_enrollment_end_date: string = "";
 	available_start_dates : string[][] = [];
 	site_short_name : string = "";
+	componentUUID : UUID;
 
 	ftes : number = 0;
 	ptes : number = 0;
@@ -23,12 +25,28 @@ export class NewPlanYearComponent {
 	benefit_packages : BenefitPackage[] = [];
 
 	constructor(private elementRef: ElementRef) {
+          this.componentUUID = UUID.UUID();
 	}
 
 	ngOnInit() {
 		var new_options : NewPlanYearOptions = JSON.parse(this.elementRef.nativeElement.getAttribute("data-angular2-new-plan-year-options"));
 		this.available_start_dates = new_options.available_start_dates;
 		this.site_short_name = new_options.site_short_name;
+	}
+
+	setCalendars(base_element_name : string, val: string) {
+		var hidden_element_id = base_element_name + "_input_" + this.componentUUID;
+		var control_element_id = base_element_name + "_control_" + this.componentUUID;
+		eval("$(\"#" + control_element_id + "\").datepicker({\n\
+                  altField: \"#" + hidden_element_id+ "\",\
+                  altFormat: \"yyyy-mm-dd\",\
+	          defaultDate: \"" + val + "\"\
+		});");
+	}
+
+	ngAfterViewChecked() {
+		this.setCalendars("open_enrollment_start_date", this.open_enrollment_start_date);
+		this.setCalendars("open_enrollment_end_date", this.open_enrollment_end_date);
 	}
 
 	planYearEndOnValue() {
