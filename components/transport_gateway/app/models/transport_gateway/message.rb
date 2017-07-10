d12757c83ea391e0b7646f57b4279aa81a77e5a7
Dan Thomas <dan.thomas@dc.gov>
2017-07-09 22:24:29 -0400

    Improve spec tests

--- components/transport_gateway/app/models/transport_gateway/message.rb
+++ components/transport_gateway/app/models/transport_gateway/message.rb
@@ -5,6 +5,21 @@ module TransportGateway
 
     attr_accessor :from, :to, :body
 
+    #  @from
+    #  @to 
+    #  @body
+
+    #  @hbx_id
+    #  @submitted_timestamp
+    #  @authorization
+    #  @service_status_type
+    #  @message_id
+    #  @originating_service
+    #  @reply_to
+    #  @fault_to
+    #  @correlation_id
+    #  @application_header_properties
+
     def initialize(**options)
       parse_options(options) unless options.empty?
 
@@ -17,23 +32,6 @@ module TransportGateway
     def to_uri(value)
       return if value.nil?
       value.is_a?(URI) ? value : URI.parse(value.to_s)
-    end
-
-    def parse_envelope_header(envelope_header)
-      @from
-      @to 
-      @body
-      
-      @hbx_id                         = envelope_header[:hbx_id] || nil
-      @submitted_timestamp            = envelope_header[:submitted_timestamp] || nil
-      @authorization                  = envelope_header[:authorization] || nil
-      @service_status_type            = envelope_header[:service_status_type] || nil
-      @message_id                     = envelope_header[:message_id] || nil
-      @originating_service            = envelope_header[:originating_service] || nil
-      @reply_to                       = envelope_header[:reply_to] || nil
-      @fault_to                       = envelope_header[:fault_to] || nil
-      @correlation_id                 = envelope_header[:correlation_id] || nil
-      @application_header_properties  = envelope_header[:application_header_properties] || nil
     end
 
     def parse_options(options)
--- components/transport_gateway/spec/models/transport_gateway/message_spec.rb
+++ components/transport_gateway/spec/models/transport_gateway/message_spec.rb
@@ -3,53 +3,81 @@ require 'rails_helper'
 module TransportGateway
   RSpec.describe Message, type: :model do
 
-    let(:from)  { "foo@example.com" }
-    let(:to)    { "bar@example.com" }
-    let(:body)  { "lorum ipsum" }
-
     context ".new" do
-      context "with no arguments" do
+      let(:source_host) { "www.example.com" }
+      let(:source_path) { "/path/to/source/folder" }
+      let(:target_host) { "www.example.com" }
+      let(:target_path) { "path/to/target/folder" }
+      let(:from)        { source_host + source_path }
+      let(:to)          { target_host + target_path }
+      let(:to_uri)      { URI.parse(to) }
+      let(:body)        { "madam in eden i'm adam" }
 
-        it "should raise an argument error" do
-          expect{ Message.new() }.to raise_error(ArgumentError) 
+      let(:valid_params) do
+        {
+          from: from,
+          to: to,
+          body: body
+        }
+      end
+
+      context "with no arguments" do
+        let(:params)  {{}}
+
+        it "should instantiate a Message instance" do
+          expect(Message.new(**params)).to be_an_instance_of(Message)
+        end
+      end
+
+      context "with no 'from' argument" do
+        let(:params)  { valid_params.except(:from) }
+
+        it "should not raise an error" do
+          expect{ Message.new(**params) }.not_to raise_error 
+        end
+      end
+
+      context "with no 'to' argument" do
+        let(:params)  { valid_params.except(:to) }
+
+        it "should not raise an error" do
+          expect{ Message.new(**params) }.not_to raise_error 
+        end
+      end
+
+      context "with no 'body' argument" do
+        let(:params)  { valid_params.except(:body) }
+
+        it "should not raise an error" do
+          expect{ Message.new(**params) }.not_to raise_error 
         end
       end
 
       context "with a unparseable URI in 'to' argument" do
-        let(:invalid_uri)  { 7463 }
+        let(:invalid_uri)  { "@7463$%^&*" }
 
         it "should raise Invalid URI error" do
-          expect{ Message.new(from, invalid_uri, body) }.to raise_error(URI::InvalidURIError) 
+          expect{ Message.new(from: from, to: invalid_uri, body: body) }.to raise_error(URI::InvalidURIError) 
         end
-      end
-
-      context "with empty 'from' argument" do
-        it "should not raise an error" do
-          expect{Message.new("", to, body)}.not_to raise_error 
-        end
-      end
+      end      
 
       context "with valid arguments" do
-        let(:params)  {valid_params}
-        let(:message_with_valid_arguments) { Message.new(from, to, body) }
-        let(:to_uri)  { URI.parse(to) }
+        let(:params)  { valid_params }
 
         it "should not raise an error" do
-          expect{message_with_valid_arguments}.not_to raise_error 
+          expect{Message.new(**params)}.not_to raise_error 
         end
 
         it "should set the instance vars" do
-          expect(message_with_valid_arguments.from).to eq from
-          expect(message_with_valid_arguments.body).to eq body
+          expect(Message.new(**params).from).to eq from
+          expect(Message.new(**params).body).to eq body
         end
 
         it "should cast the 'to' argument as a URI" do
-          expect(message_with_valid_arguments.to).to eq to_uri
+          expect(Message.new(**params).to).to eq to_uri
         end
-
       end
 
     end
-
   end
 end
