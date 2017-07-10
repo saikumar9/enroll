f97bf4bf3601c4ae858d4dbf7d886e1a8b5fb4d1
Dan Thomas <dan.thomas@dc.gov>
2017-07-10 08:53:49 -0400

    Add #select_payload.  Simplify http setup and request call

--- components/transport_gateway/app/models/transport_gateway/adapters/http_adapter.rb
+++ components/transport_gateway/app/models/transport_gateway/adapters/http_adapter.rb
@@ -5,45 +5,47 @@ module TransportGateway
   class Adapters::HttpAdapter
 
     def send_message(message)
-      resource    = message.to
+      target_uri  = message.to
+      source      = message.from
       body        = message.body
-      put_request = request_for(resource, body)
 
-      http_site(resource) do |http, uri|
-        http.request(put_request)
-      end
+      payload = select_payload(source, body)
+      put_request = request_for(target_uri, payload)
+
+      http_site = Net::HTTP.new(target_uri.host, target_uri.port)
+      http_site.request(put_request)
     end
 
   private
 
-    def request_for(uri, body)
+    def select_payload(source, body)
+      return source if (source.present? && source.is_a?(File))
+      return body if body.present?
+      nil
+    end
+
+    def request_for(uri, content)
       request = Net::HTTP::Put.new(uri.path)
       request.basic_auth(uri.user, uri.password) unless uri.userinfo.blank?
 
-      request_body_for(request, body)
+      request_content_type_for(request, content)
     end
 
-    def request_body_for(request, content)
-
+    def request_content_type_for(request, content)
       case content
+       when File 
+        request.set_content_type('text/plain')
+        request.body_stream = content
       when String
         request.set_content_type('text/plain')
         request.body = content
-      when File
-        request.set_content_type('text/plain')
-        request.body_stream = content
       when nil
         request.body = nil
       else
         request.body = content
       end
-      request
-    end
 
-    def http_site(uri)
-      Net::HTTP.start(uri.host, uri.port) do |http|
-        yield(http, uri)
-      end
+      request
     end
 
   end
