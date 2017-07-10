e5daf4213901935ec289ca617b0e1bb06341f02a
Dan Thomas <dan.thomas@dc.gov>
2017-07-10 08:41:36 -0400

    initial commit

---
+++ components/transport_gateway/spec/models/transport_gateway/credentials/example_dot_com_credential_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+module TransportGateway
+  RSpec.describe Credentials::ExampleDotComCredential, type: :model do
+    pending "add some examples to (or delete) #{__FILE__}"
+  end
+end
