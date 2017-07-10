4f8d8147742832bdbc5f3e73cd9937734329ceed
Dan Thomas <dan.thomas@dc.gov>
2017-07-10 09:02:26 -0400

    Disable sftp_adapter spec.  Need test harness for SFTP protocol

--- components/transport_gateway/spec/models/transport_gateway/gateway_spec.rb
+++ components/transport_gateway/spec/models/transport_gateway/gateway_spec.rb
@@ -7,9 +7,9 @@ module TransportGateway
     let(:sftp_uri)      { URI("sftp://foo:bar@sftp.example.com/path/to/file;type=b") } 
     let(:binary_file)   {  }
 
-    context "sends messages and payloads over various protocols using a common interface" do
+    context "A common interface for sending messages with payloads using various protocols" do
 
-      context "when a TransportGateway is initialized" do
+      context "when a Gateway is initialized" do
         # let(:gateway)   { TransportGateway::TransportGateway.new() }
 
         it "should load an SFTP adapter" do
@@ -29,7 +29,7 @@ module TransportGateway
         end
 
 
-        context "and a file-type source is specified" do
+        context "and the message source is a File" do
           let(:source_file_path)    { File.join(File.expand_path("../../..", __FILE__), "test_files", "text_file.txt") }
           let(:source_file_handle)  { File.new(source_file_path) }
           let(:source_file_data)    { File.read(source_file_path) }
@@ -38,7 +38,7 @@ module TransportGateway
             expect(File.read(source_file_path)).to eq source_file_data
           end
 
-          context "and a message is sent using SFTP protocol" do
+          context "and the message is sent using SFTP protocol" do
             let(:target_host)     { "ftp.example.com" }
             let(:target_folder)   { "/path/to/target/folder" }
             let(:target_user)     { "foo" }
@@ -50,13 +50,12 @@ module TransportGateway
             let(:to)              { URI::Generic.build({ scheme: scheme, host: target_host, path: target_folder, userinfo: target_userinfo }) } 
             let(:message)         { Message.new(from: from, to: to) }
 
-            it "should successfully send the file" do
-              expect(Gateway.new().send_message(message)).to eq "worked!"
-            end
+            it "should successfully send the file" 
+              # expect(Gateway.new().send_message(message)).to eq "worked!"
 
           end
 
-          context "and a message is sent using HTTP protocol" do
+          context "and the message is sent using HTTP protocol" do
             let(:target_host)       { "www.example.com" }
             let(:target_service)    { "/path/to/target/service" }
             let(:target_user)       { "foo" }
@@ -88,38 +87,6 @@ module TransportGateway
           end
         end
       end
-
-      #     let(:userinfo)  { "foo:bar" }
-      #     let(:from)      { URI::FTP.build({ host: source_host, path: source_path }) } 
-      #     let(:to)        { URI::FTP.build({ host: target_host, path: target_path, userinfo: userinfo }) }
-      #     let(:body)      { nil }
-
-      #     let(:valid_params) do
-      #       {
-      #         from: from,
-      #         to: to,
-      #         body: body
-      #       }
-      #     end
-
-      #     context "with no arguments" do
-      #       let(:params)  {{}}
-      #       let(:message) { Message.new(**params)}
-
-      #       it "should raise an Argument error" do
-      #         expect{ Adapters::SftpAdapter.new.send_message(message) }.to raise_error(ArgumentError, /source file not provided/) 
-      #       end
-      #     end
-
-      #     context "with no source file (message.from)" do
-      #       let(:params)  { valid_params.except(:from) }
-      #       let(:message) { Message.new(**params)}
-
-      #       it "should raise an Argument error" do
-      #         expect{ Adapters::SftpAdapter.new.send_message(message) }.to raise_error(ArgumentError, /source file not provided/) 
-      #       end
-      #     end
-      # end
 
     end
   end
