f11ec56eea0e125486c0386867c18f7f3657486f
Brian Weiner <brian.weiner@dc.gov>
2017-07-20 11:43:17 -0400

    ref#16011 refactor devise related auth behaviors into separated concern

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def lockable
     user = User.find(params[:id])
     authorize User, :lockable?
-    user.update_lockable
+    user.lock!
     redirect_to user_account_index_exchanges_hbx_profiles_url, notice: "User #{user.person.full_name} is successfully #{user.lockable_notice}."
   rescue Exception => e
     redirect_to user_account_index_exchanges_hbx_profiles_url, alert: "You are not authorized for this action."
