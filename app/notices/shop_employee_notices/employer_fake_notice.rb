1a265a8b5d7d36e4cf8bc9638e128514768770da
Prem Shaganti <prem.shaganti@dc.gov>
2017-07-25 15:02:26 -0400

    for demo this is manullay we loading data

---
+++ app/notices/shop_employee_notices/employer_fake_notice.rb
@@ -0,0 +1,14 @@
+class ShopEmployeeNotices::EmployerFakeNotice < ShopEmployeeNotice
+
+  def deliver
+    build
+    upload_and_send_secure_message
+  end
+
+  def build
+    pdf = CombinePDF.new
+    pdf << CombinePDF.load(Rails.root.join('lib/pdf_templates', 'Denial of Initial Employer Application and Request for Clarifying Documentation.pdf'))
+    pdf.save "tmp/prem-fake.pdf"
+  end
+
+end
