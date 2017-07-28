c8290bdbf74d71350ab5a37d7da2f3692a032c3c
Dan Thomas <dan.thomas@dc.gov>
2017-07-28 13:26:43 -0400

    Mount engine in Enroll App

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   mount TransportGateway::Engine, at: "/transport_gateway"
+  mount Notifier::Engine, at: "/notifier" 
 
   require 'resque/server'
   mount Resque::Server, at: '/jobs'
