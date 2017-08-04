c5fc7c521534e73267e487c1377902d39cb8dc4d
Brian Weiner <brian.weiner@dc.gov>
2017-08-04 12:21:50 -0400

    ref#17597 use curam_enabled? helper to hide oim_id specific behaviors, simplify load_captcha_widget? method
     Conflicts:
    	app/helpers/config/site_helper.rb conflicts on line 31-41, kept both changes

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -32,6 +32,10 @@ module Config::SiteHelper
     link_to "#{Settings.aca.state_name} #{Settings.site.short_name} Broker Agreement", Settings.site.terms_and_conditions_url
   end
 
+  def site_curam_enabled?
+    Settings.site.curam_enabled
+  end
+
   def site_home_link
     link_to site_home_url, site_home_url
   end
