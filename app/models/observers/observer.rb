cd17b31ef4567f4d34907bf24d5a5cb1a2e6650d
Dan Thomas <dan.thomas@dc.gov>
2017-08-03 21:27:44 -0400

    Toying with event definitions

--- app/models/observers/observer.rb
+++ app/models/observers/observer.rb
@@ -2,15 +2,23 @@ require "Observer"
 class Observers::Observer
   include Observable
 
-  attr_reader :event_name, :obj, :options
+  EVENTS = [
+    advance_day:          { models: [:time_keeper], settings_key: nil,  },
+    state_change:         { }, 
+    broker_agency_change: { }, 
+    general_agent_change: { },
+    benefit_period_start: { models: [:employer_profile] },
+  ]
+
+  attr_reader :event_name, :klass_instance, :options
 
   # "update" is the generic Observable event notification hook method name.
   # By convention, event notification method names will be prepended with the 
   # class name, e.g. employer_profile_update
 
-  def update(event_name, obj, options = {})
+  def update(event_name, klass_instance, options = {})
     @event_name = event_name
-    @obj = obj
+    @klass_instance = klass_instance
 
     options.each { |k, v| @k = v }
   end
