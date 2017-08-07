dc7748d0199656ad5ba3c53693dc09383d1cd257
Dan Thomas <dan.thomas@dc.gov>
2017-08-07 12:55:38 -0400

    Add market and site key constants

--- components/transport_gateway/lib/transport_gateway.rb
+++ components/transport_gateway/lib/transport_gateway.rb
@@ -1,6 +1,10 @@
 require "transport_gateway/engine"
 require "mongoid" 
 require "slim"
+require "uri/aws3"
 
 module TransportGateway
+  MARKET_KINDS  = [:aca_shop, :aca_individual, :aca_congress, :any]
+  SITE_KEYS     = [:dchbx, :cca]
+
 end
