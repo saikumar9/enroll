987328d870a2da78fbf47a2b7f09ad3d1b7a032a
saip7795 <saip7795@gmail.com>
2017-08-10 15:32:33 -0400

    Ref 17062 Checking for other method as well

--- app/helpers/config/site_helper.rb
+++ app/helpers/config/site_helper.rb
@@ -59,6 +59,10 @@ module Config::SiteHelper
     Settings.site.use_default_devise_path
   end
 
+  def site_check
+    Settings.site.site_check
+  end
+
   def find_your_doctor_url
     Settings.site.shop_find_your_doctor_url
   end
