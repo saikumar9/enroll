629500dc101e4e881ff32d2bb6e85e7d8b761b93
vsonnathi <sonnathivinil@gmail.com>
2017-08-15 10:29:24 -0400

    Refs #17799: Show carrier contact info

--- spec/factories/carrier_profiles.rb
+++ spec/factories/carrier_profiles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :carrier_profile do
-    organization  { FactoryGirl.create(:organization, legal_name: "United Health Care", dba: "United") }
+    organization  { FactoryGirl.create(:organization, legal_name: "BMC HealthNet Plan", dba: "BMC HealthNet Plan") }
     abbrev        "UHIC"
     offers_sole_source false
     issuer_hios_ids ['11111']
