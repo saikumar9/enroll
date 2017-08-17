fd8eb0fbf0bfd53aaa630ab7dd28ad96d1130eb9
harsha chalamala <chvreddy1015@gmail.com>
2017-08-17 17:40:22 -0400

    refs # 17506 unwante changes
     Conflicts:
    	db/seedfiles/notice_triggers_seed.rb conflicts on line 736-932, kept both changes

--- db/seedfiles/notice_triggers_seed.rb
+++ db/seedfiles/notice_triggers_seed.rb
@@ -923,6 +923,28 @@ shop_notice_triggers = [
         }
       ]
     },
+
+    {
+      hbx_id: 'SHOP_M029',
+      title: 'CONFIRMATION OF ELECTION TO WAIVE COVERAGE ',
+      description: 'Employee waiver confirmation',
+      resource_name: 'employee_role',
+      event_name: 'employee_waiver_notice',
+      notice_triggers: [
+        {
+            name: 'Notice to employee after they select a plan Annual Open Enrollment',
+            notice_template: 'notices/shop_employee_notices/employee_waiver_confirmation_notification',
+            notice_builder: 'ShopEmployeeNotices::EmployeeWaiverConfirmNotice',
+            mpi_indicator: 'SHOP_M029',
+            notice_trigger_element_group: {
+                market_places: ['shop'],
+                primary_recipients: ["employee"],
+                primary_recipient_delivery_method: ["secure_message"],
+                secondary_recipients: []
+            }
+        }
+      ]
+    },
 ]
 
 ivl_notice_triggers = [
