dc3576dcfed0fd3aae2955c5ebb2648a6beb648b
harsha chalamala <chvreddy1015@gmail.com>
2017-08-17 17:37:44 -0400

    refs #17506 waiver notice

--- db/seedfiles/notice_triggers_seed.rb
+++ db/seedfiles/notice_triggers_seed.rb
@@ -519,6 +519,48 @@ shop_notice_triggers = [
             }
         ]
     },
+    {
+      hbx_id: 'SHOP_M070',
+      title: 'Employee Enrollment Confirmation',
+      description: 'Employee selects a plan during annual open enrollment OE is still close and final confirmation',
+      resource_name: 'employee_role',
+      event_name: 'initial_employee_plan_selection_confirmation',
+      notice_triggers: [
+        {
+            name: 'Notice to employee after they select a plan Annual Open Enrollment',
+            notice_template: 'notices/shop_employee_notices/initial_employee_plan_selection_confirmation',
+            notice_builder: 'ShopEmployeeNotices::InitialEmployeePlanSelectionConfirmation',
+            mpi_indicator: 'SHOP_M070',
+            notice_trigger_element_group: {
+                market_places: ['shop'],
+                primary_recipients: ["employee"],
+                primary_recipient_delivery_method: ["secure_message"],
+                secondary_recipients: []
+            }
+        }
+      ]
+    },
+    {
+      hbx_id: 'SHOP_M029',
+      title: 'CONFIRMATION OF ELECTION TO WAIVE COVERAGE ',
+      description: 'Employee waiver confirmation',
+      resource_name: 'employee_role',
+      event_name: 'employee_waiver_notice',
+      notice_triggers: [
+        {
+            name: 'Notice to employee after they select a plan Annual Open Enrollment',
+            notice_template: 'notices/shop_employee_notices/employee_waiver_confirmation_notification',
+            notice_builder: 'ShopEmployeeNotices::EmployeeWaiverConfirmNotice',
+            mpi_indicator: 'SHOP_M029',
+            notice_trigger_element_group: {
+                market_places: ['shop'],
+                primary_recipients: ["employee"],
+                primary_recipient_delivery_method: ["secure_message"],
+                secondary_recipients: []
+            }
+        }
+      ]
+    },
 ]
 
 ivl_notice_triggers = [
