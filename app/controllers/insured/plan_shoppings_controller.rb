c207f16a6731e899e9404b7bb9dcb1e1347fb56b
saip7795 <saip7795@gmail.com>
2017-09-06 11:13:13 -0400

    Ref17810: Change in trigger location

--- app/controllers/insured/plan_shoppings_controller.rb
+++ app/controllers/insured/plan_shoppings_controller.rb
@@ -57,6 +57,7 @@ class Insured::PlanShoppingsController < ApplicationController
 
     @change_plan = params[:change_plan].present? ? params[:change_plan] : ''
     @enrollment_kind = params[:enrollment_kind].present? ? params[:enrollment_kind] : ''
+    employee_mid_year_plan_change(@person) if @change_plan.present? or @enrollment_kind.present?
 
     send_receipt_emails if @person.emails.first
   end
@@ -133,6 +134,17 @@ class Insured::PlanShoppingsController < ApplicationController
     rescue Exception => e
       puts "Unable to send Employee Waiver notice to #{census_employee.full_name}" unless Rails.env.test?
     end
+  end
+
+    def employee_mid_year_plan_change(person)
+     begin
+          employee_role_id = person.active_employee_roles.first.census_employee.id
+          if employee_role_id.present?
+         ShopNoticesNotifierJob.perform_later(employee_role_id.to_s, "employee_mid_year_plan_change")
+       end
+     rescue Exception => e
+       log("#{e.message}; person_id: #{person.id}")
+     end
   end
 
   def terminate
