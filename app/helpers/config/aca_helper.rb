c36243a440e8b8c0a259ceb31abbb085c7999099
Prem Shaganti <prem.shaganti@dc.gov>
2017-10-10 23:36:51 -0400

    ref:18751 slight changes on carrier choosing while employer plan year purchase

--- app/helpers/config/aca_helper.rb
+++ app/helpers/config/aca_helper.rb
@@ -60,6 +60,18 @@ module Config::AcaHelper
     @offers_nationwide_plans ||= Settings.aca.nationwide_markets
   end
 
+  def fetch_plan_title_for_sole_source
+    Settings.plan_option_titles.sole_source
+  end
+
+  def fetch_plan_title_for_metal_level
+    Settings.plan_option_titles.metal_level
+  end
+
+  def fetch_plan_title_for_single_carrier
+    Settings.plan_option_titles.single_carrier
+  end
+
   def carrier_special_plan_identifier_namespace
     @carrier_special_plan_identifier_namespace ||= Settings.aca.carrier_special_plan_identifier_namespace
   end
