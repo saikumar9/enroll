22b0cdcdc235ececbcee243bee79d394a0eeb941
Brian Weiner <brian.weiner@dc.gov>
2017-10-20 14:58:58 -0400

    ref#19145 use appropriate carrier_service_area method to restrict returned areas within active year
     Conflicts:
    	app/models/queries/employer_plan_offering_strategies/for_service_area.rb
    took branhc changes on 18-20

--- spec/models/carrier_profile_spec.rb
+++ spec/models/carrier_profile_spec.rb
@@ -12,6 +12,20 @@ RSpec.describe CarrierProfile, :type => :model do
   let(:organization) {FactoryGirl.create(:organization)}
   let(:abbrev) {"uhc"}
 
+  describe "class methods" do
+    context "carrier_profile_service_area_pairs_for" do
+      let!(:carrier_profile) { create(:carrier_profile, with_service_areas: 0) }
+      let!(:carrier_service_area_2017) { create(:carrier_service_area, service_area_id: 'TEST-2017', issuer_hios_id: carrier_profile.issuer_hios_ids.first, active_year: '2017') }
+      let!(:carrier_service_area_2018) { create(:carrier_service_area, service_area_id: 'TEST-2018', issuer_hios_id: carrier_profile.issuer_hios_ids.first, active_year: '2018') }
+      let!(:employer) { create(:employer_profile) }
+
+      it "should return the appropriate service area based on year" do
+        expect(CarrierProfile.carrier_profile_service_area_pairs_for(employer, '2017' )).to contain_exactly([carrier_profile.id, 'TEST-2017'])
+        expect(CarrierProfile.carrier_profile_service_area_pairs_for(employer, '2018' )).to contain_exactly([carrier_profile.id, 'TEST-2018'])
+      end
+    end
+  end
+
   describe ".new" do
 
     let(:valid_params) do
