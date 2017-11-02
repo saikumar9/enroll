d220b03e5e4f6dceade1e569349af6a50ac3255e
Brian Weiner <brian.weiner@dc.gov>
2017-11-02 11:51:28 -0400

    fixes

--- spec/factories/carrier_service_area.rb
+++ spec/factories/carrier_service_area.rb
@@ -9,7 +9,13 @@ FactoryGirl.define do
     state_code nil
     service_area_zipcode nil
     partial_county_justification nil
-    active_year { TimeKeeper.date_of_record.year + 1 }
+    active_year {
+      current_time = TimeKeeper.date_of_record
+      if ((12 + Settings.aca.shop_market.initial_application.earliest_start_prior_to_effective_on.months) < current_time.month)
+        current_time = current_time + 1.year
+      end
+      current_time.year
+    }
 
     trait :for_partial_state do
       service_area_name 'Partial State Area'
