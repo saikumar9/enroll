8180d4950593bd011057ea7ddf7e98bddb4881b7
Nikhil S <nikhilks219@gmail.com>
2017-11-03 15:17:56 -0400

    18941: changed event name reference for plan year

--- app/models/observers/notice_observer.rb
+++ app/models/observers/notice_observer.rb
@@ -1,24 +1,10 @@
 module Observers
   class NoticeObserver < Observer
-
-    PLANYEAR_NOTICE_EVENTS = [
-      :renewal_application_created,
-      :initial_application_submitted,
-      :renewal_application_submitted,
-      :renewal_application_autosubmitted,
-      :ineligible_initial_application_submitted,
-      :ineligible_renewal_application_submitted,
-      :open_enrollment_began,
-      :open_enrollment_ended,
-      :application_denied,
-      :renewal_application_denied
-    ]
-
     
     def plan_year_update(new_model_event)
       raise ArgumentError.new("expected ModelEvents::ModelEvent") unless new_model_event.is_a?(ModelEvents::ModelEvent)
 
-      if PLANYEAR_NOTICE_EVENTS.include?(new_model_event.event_key)
+      if PlanYear::REGISTERED_EVENTS.include?(new_model_event.event_key)
         plan_year = new_model_event.klass_instance
         
         if new_model_event.event_key == :renewal_application_denied
