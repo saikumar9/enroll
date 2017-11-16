7f6b5580950ce92dbc171620302dd79bd446803f
harsha chalamala <chvreddy1015@gmail.com>
2017-11-16 13:31:22 -0500

    ref 19014 employer attesttation factory changed

--- spec/factories/employer_attestation_document.rb
+++ spec/factories/employer_attestation_document.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :employer_attestation_document do
     employer_attestation
+    identifier "urn:openhbx:terms:v1:file_storage:s3:bucket:mhc-enroll-attestations-qa#843567864-8260-56-9b563488-381e4436"
+    title "Document #{Forgery('basic').text(:allow_lower   => false,
+                                            :allow_upper   => false,
+                                            :allow_numeric => true,
+                                            :allow_special => false, :exactly => 1)}"
   end
 end
