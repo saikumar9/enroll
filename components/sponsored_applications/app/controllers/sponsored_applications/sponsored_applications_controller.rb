c2644f97d64fe4afd1fde91bcb758866d0febcec
Brian Weiner <brian.weiner@dc.gov>
2017-12-12 14:59:17 -0500

    ref#19401 basic route to create a quote for employer_profile and broker

--- components/sponsored_applications/app/controllers/sponsored_applications/sponsored_applications_controller.rb
+++ components/sponsored_applications/app/controllers/sponsored_applications/sponsored_applications_controller.rb
@@ -4,5 +4,19 @@ module SponsoredApplications
     def index
     end
 
+    def new
+    end
+
+    private
+    helper_method :broker_agency_profile, :sponsor
+
+    def broker_agency_profile
+      @broker_agency_profile ||= BrokerAgencyProfile.find(params[:broker_id])
+    end
+
+    def sponsor
+      @employer_profile ||= EmployerProfile.find(params[:sponsor_id])
+    end
+
   end
 end
--- components/sponsored_applications/config/routes.rb
+++ components/sponsored_applications/config/routes.rb
@@ -1,3 +1,4 @@
 SponsoredApplications::Engine.routes.draw do
+  get "broker/:broker_id/sponsor/:sponsor_id", to: "sponsored_applications#new", as: "broker_quote_creation"
   resources :sponsored_applications
 end
