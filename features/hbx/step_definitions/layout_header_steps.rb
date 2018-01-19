848c75060e1a393cb19d700701b9c98ae588f86e
harsha280 <harsha.vskr@gmail.com>
2018-01-19 13:24:41 -0500

    Refs 17871 ajax line included

--- features/hbx/step_definitions/layout_header_steps.rb
+++ features/hbx/step_definitions/layout_header_steps.rb
@@ -3,5 +3,6 @@ When(/^the Hbx Admin clicks on the Ma logo link$/) do
 end
 
 Then(/^it should redirect to Ma Health connector website$/) do
+  wait_for_ajax(3,2)
   expect(page).to have_content("Health Connector for Business")
 end
