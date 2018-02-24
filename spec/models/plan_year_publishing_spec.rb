96162135546642b193cfd3f24aff295898c2a7f0
raghuram <raghuramg83@gmail.com>
2018-02-24 16:04:30 -0500

    Refs #22519 Updated specs to use transmission date as threshold date

--- spec/models/plan_year_publishing_spec.rb
+++ spec/models/plan_year_publishing_spec.rb
@@ -7,6 +7,8 @@ describe PlanYear, "that is:
 " do
 
   let(:employer_profile) { EmployerProfile.new(:aasm_state => "binder_paid") }
+  let(:threshold_day) { Settings.aca.shop_market.employer_transmission_day_of_month }
+  let(:monthly_oe_end_on) { Settings.aca.shop_market.open_enrollment.monthly_end_on }
 
   before :each do
     allow(PlanYear).to receive(:transmit_employers_immediately?).and_return(false)
@@ -16,14 +18,14 @@ describe PlanYear, "that is:
   subject do
     PlanYear.new({
       :aasm_state => "enrolled",
-      :open_enrollment_end_on => Date.new(2017, 6, 15),
+      :open_enrollment_end_on => Date.new(2017, 6, monthly_oe_end_on),
       :start_on => Date.new(2017, 7, 1),
       :employer_profile => employer_profile
     })
   end
 
-  describe "and has reached the 15th" do
-    let(:current_date) { Date.new(2017, 6, 15) }
+  describe "and has not reached threshold date" do
+    let(:current_date) { Date.new(2017, 6, threshold_day - 1) }
 
     it "is NOT eligible for export" do
       expect(subject.eligible_for_export?).to be_falsey
@@ -31,8 +33,8 @@ describe PlanYear, "that is:
 
   end
 
-  describe "and has reached the 16th" do
-    let(:current_date) { Date.new(2017, 6, 16) }
+  describe "and has reached threshold date" do
+    let(:current_date) { Date.new(2017, 6, threshold_day) }
 
     it "is eligible for export" do
       expect(subject.eligible_for_export?).to be_truthy
@@ -48,16 +50,18 @@ describe PlanYear, "that is:
 " do
 
   let(:employer_profile) { EmployerProfile.new(:aasm_state => "binder_paid") }
+  let(:monthly_oe_end_on) { Settings.aca.shop_market.open_enrollment.monthly_end_on }
+  let(:threshold_day) { Settings.aca.shop_market.employer_transmission_day_of_month }
 
   before :each do
     allow(PlanYear).to receive(:transmit_employers_immediately?).and_return(true)
-    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,15))
+    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,threshold_day-1))
   end
 
   subject do
     PlanYear.new({
       :aasm_state => "enrolled",
-      :open_enrollment_end_on => Date.new(2017, 6, 14),
+      :open_enrollment_end_on => Date.new(2017, 6, monthly_oe_end_on),
       :start_on => Date.new(2017, 7, 1),
       :employer_profile => employer_profile
     })
@@ -70,21 +74,23 @@ end
 
 describe PlanYear, "that is:
 - finished renewal open enrollment
-- is configured to wait for the 15th of the month
-- has reached the 16th
+- is configured to wait for the threshold day of the month
+- has reached the threshold date
 " do
 
-  let(:employer_profile) { EmployerProfile.new(:aasm_state => "binder_paid") }
+  let(:employer_profile) { EmployerProfile.new(:aasm_state => "enrolled") }
+  let(:monthly_oe_end_on) { Settings.aca.shop_market.renewal_application.monthly_open_enrollment_end_on }
+  let(:threshold_day) { Settings.aca.shop_market.employer_transmission_day_of_month }
 
   before :each do
-    allow(PlanYear).to receive(:transmit_employers_immediately?).and_return(true)
-    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,16))
+    allow(PlanYear).to receive(:transmit_employers_immediately?).and_return(false)
+    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,threshold_day))
   end
 
   subject do
     PlanYear.new({
       :aasm_state => "renewing_enrolled",
-      :open_enrollment_end_on => Date.new(2017, 6, 15),
+      :open_enrollment_end_on => Date.new(2017, 6, monthly_oe_end_on),
       :start_on => Date.new(2017, 7, 1),
       :employer_profile => employer_profile
     })
@@ -97,20 +103,22 @@ end
 
 describe PlanYear, "that is:
 - finished renewal open enrollment
-- is *NOT* configured to wait for the 15th of the month
+- is *NOT* configured to wait for the threshold day of the month
 " do
 
-  let(:employer_profile) { EmployerProfile.new(:aasm_state => "binder_paid") }
+  let(:employer_profile) { EmployerProfile.new(:aasm_state => "enrolled") }
+  let(:threshold_day) { Settings.aca.shop_market.employer_transmission_day_of_month }
+  let(:monthly_oe_end_on) { Settings.aca.shop_market.renewal_application.monthly_open_enrollment_end_on }
 
   before :each do
     allow(PlanYear).to receive(:transmit_employers_immediately?).and_return(true)
-    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,15))
+    allow(TimeKeeper).to receive(:date_of_record).and_return(Date.new(2017,6,threshold_day-1))
   end
 
   subject do
     PlanYear.new({
       :aasm_state => "renewing_enrolled",
-      :open_enrollment_end_on => Date.new(2017, 6, 14),
+      :open_enrollment_end_on => Date.new(2017, 6, monthly_oe_end_on),
       :start_on => Date.new(2017, 7, 1),
       :employer_profile => employer_profile
     })
