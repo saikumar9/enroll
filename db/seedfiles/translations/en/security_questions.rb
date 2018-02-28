9e1e28009bdb00b5ffb4d8d3e93ebc49ba846753
ymhari <hariym471@gmail.com>
2018-02-28 11:36:51 -0500

    ref: 18124 Adds localization

---
+++ db/seedfiles/translations/en/security_questions.rb
@@ -0,0 +1,7 @@
+# Devise Translations
+SECURITY_QUESTIONS_TRANSLATIONS = {
+    "en.users.security_questions_responses.secure_your_account" => "Please helps us secure your account",
+    "en.users.security_questions_responses.respond_for_three_questions" => "Please take a moment to provide security responses for three choices from our available options",
+    "en.users.security_questions_responses.questions_will_help_us_confirm" => "These questions help us confirm your identity if you forget your password, or if your account behaves suspiciously and is locked.",
+    "en.users.security_questions_responses.please_select_a_question" => "Please Select A Question"
+}
